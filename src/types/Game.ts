b1ec53e8f6f8e8774414e51585746776f992462a
FrigoAlex <alejo012903@hotmail.com>
2023-03-16 22:08:35 -0500

    🏷️ Add detailed game interface

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -7,3 +7,14 @@ export interface Game {
     platforms: string[];
     genres: string[];
 }
+
+export interface DetailedGame{
+    name: string;
+    releaseDate: string;
+    image: string;
+    rating: number;
+    author: undefined|string 
+    platforms: string[];
+    genres: string[];
+    description: string;
+}
