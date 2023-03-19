fbdfab73a4ae449016c9c8cd3187dd13cd1d5a7f
FrigoAlex <alejo012903@hotmail.com>
2023-03-19 17:59:19 -0500

    🚚🔧  Rename and update PageRoutes to ReactRouter

---
+++ src/constants/pageRoutes.ts
@@ -0,0 +1,11 @@
+/*  ---------------Routes-------------------   */
+export enum PageRoutes {
+    HOME = "/",
+    GAME ="/game",
+    LOGIN = "/login",
+    REGISTER = "/register",
+}
+/*  ---------------Params-------------------   */
+export enum Params{
+    GAME_ID = "/:gameId",
+}
--- src/constants/pageRoutes.tsx
+++
@@ -1,5 +0,0 @@
-/*  ---------------Routes-------------------   */
-export const HOME = "^/$";
-export const GAME = "^/game/\\d+$";
-export const LOGIN = "^/login$";
-export const REGISTER = "^/register$";
