bef08413e6dc689fd05054afcaf438176cd05dde
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 08:18:03 -0500

    🏗️ Implement react-router-dom into the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import "./assets/styles/App.css";
-import useNavigate from "./hooks/useNavigate";
 import Navbar from "./components/Navbar";
-import GamesSection from "./pages/gamesSection";
-import GameDescription from "./pages/gameDescription";
+import GamesSection from "./pages/GamesSection";
+import GameDescription from "./pages/GameDescription";
 import Footer from "./components/Footer";
 import NoPage from "./components/NoPage";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import { HOME, GAME, LOGIN, REGISTER } from "./constants/pageRoutes";
-import useSession from "./hooks/useSession";
-function App () {
-  const { path, navigateTo } = useNavigate();
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { PageRoutes, Params } from "./constants/pageRoutes";
+import useSession from "./hooks/useUser"
+
+const App = () => {
   const { user, login, logOut, errors } = useSession();
   return (
-    <div className="App">
-      <Navbar navigateTo={navigateTo} user={user} logout={logOut} />
-      {
-        [{ url: HOME, page: <GamesSection key={0} navigateTo={navigateTo} /> },
-          { url: GAME, page: <GameDescription key={1} user={user}/> },
-          { url: LOGIN, page: <Login login={login} errors={errors} user={user} navigateTo={navigateTo} key={2} /> },
-          { url: REGISTER, page: <Register navigateTo={navigateTo} key={3} /> }
-        ].find((x) => path.match(new RegExp(x.url)))?.page || <NoPage/>
-      }
+    <Router>
+      <Navbar user={user} logout={logOut} />
+      <Routes>
+        <Route path={PageRoutes.HOME} element={<GamesSection />} />
+        <Route path={PageRoutes.GAME+Params.GAME_ID} element={<GameDescription user={user}/>}/>
+        <Route path={PageRoutes.LOGIN} element={<Login errors={errors} login={login} user={user}/>} />
+        <Route path={PageRoutes.REGISTER} element={<Register/>} />
+        <Route path="*" element={<NoPage />}/>
+      </Routes>
       <Footer />
-    </div>
-  );
-}
-
+    </Router>
+  )
+};
 export default App;
