aecb4f5225e494219cb8476b12cd9562c59f2cfd
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 08:46:23 -0500

    ♻️ Update the navbar to work with react-router

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,44 @@
+import { useNavigate } from "react-router-dom"; 
 import "../assets/styles/navbar.css";
+import { User } from "../types/user"; 
+import {PageRoutes} from "../constants/pageRoutes"
+interface NavbarProps {
+  user: User|undefined;
+  logout: () =>void
+}
 
-const Navbar = ({ navigateTo, user, logout }) => {
+
+const Navbar = ({ user, logout }: NavbarProps) => {
+  const navigate = useNavigate();
   return (
-    <header className="navbar">
-      <div className="logo-container" onClick={() => navigateTo("/")}>
+    <>
+     <header className="navbar">
+      <div className="logo-container" onClick={() => {
+        navigate(PageRoutes.HOME);
+      }}>
         <img className="header-logo" src="/Logo-Gamer.png" />
         <h1 className="header-title">Gamer Corner</h1>
       </div>
       <nav className="nav-section">
         <ul className="nav-list">
-          <li className="nav-elements" onClick={() => navigateTo("/")}>
+          <li className="nav-elements" onClick={() => {
+            navigate(PageRoutes.HOME);
+          }}>
             <a className="nav-elements-a">Home</a>
           </li>
           {!user
             ? (
               <>
-                <li className="nav-elements" onClick={() => navigateTo("/login")}>
+                <li className="nav-elements" onClick={() => {
+                  navigate("/login");
+                }}>
                   <a className="nav-elements-a">Login</a>
                 </li>
                 <li
                   className="nav-elements"
-                  onClick={() => navigateTo("/register")}
+                  onClick={()=>{
+                    navigate("/register");
+                  }}
                 >
                   <a className="nav-elements-a">Register</a>
                 </li>
@@ -29,14 +47,15 @@ const Navbar = ({ navigateTo, user, logout }) => {
             : (
               <li className="nav-elements" onClick={async () => {
                 await logout();
-                navigateTo("/");
+                navigate("/");
               }}>
                 <a className="nav-elements-a">logout</a>
               </li>
             )}
         </ul>
       </nav>
-    </header>
-  );
+    </header> 
+</>
+  )
 };
 export default Navbar;
