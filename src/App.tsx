b6499d61fce21141ddd832461700dbae49fe5fe3
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 23:22:29 -0500

    🐛 Fix gameID routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import "./assets/styles/App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import GamesSection from "./pages/GamesSection";
 import GameDescription from "./pages/GameDescription";
@@ -6,13 +11,11 @@ import Footer from "./components/Footer";
 import NoPage from "./components/NoPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import PrivateRoutes from "./components/privateRoutes";
 import { PageRoutes, Params } from "./constants/pageRoutes";
 import useSession from "./hooks/useUser"
+
+import "./assets/styles/App.css";
 
 const App = () => {
   const { user, login, logOut, errors } = useSession();
@@ -21,9 +24,11 @@ const App = () => {
       <Navbar user={user} logout={logOut} />
       <Routes>
         <Route path={PageRoutes.HOME} element={<GamesSection />} />
-        <Route path={PageRoutes.GAME+Params.GAME_ID} element={<GameDescription user={user}/>}/>
+        <Route path={PageRoutes.GAME} element={<PrivateRoutes user={user}/>}> 
+          <Route path={PageRoutes.GAME+Params.GAME_ID} element={<GameDescription user={user}></GameDescription>}/>
+        </Route>
         <Route path={PageRoutes.LOGIN} element={<Login errors={errors} login={login} user={user}/>} />
-        <Route path={PageRoutes.REGISTER} element={<Register/>} />
+        <Route path={PageRoutes.REGISTER} element={<Register/>} />  
         <Route path="*" element={<NoPage />}/>
       </Routes>
       <Footer />
