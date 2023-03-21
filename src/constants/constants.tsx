8eabb05eb95fee99ad7c28b7ce83821ea1078fe3
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 22:49:26 -0500

    🔧 Add Reducers enums into constants

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -3,5 +3,19 @@ export const API_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const USER_API_URL = import.meta.env.VITE_USER_API_URL;
 
-/*  -------------Debounce Timer----------------   */
+/*  -------------DEBOUNCE TIMER----------------   */
 export const debounceTimer = 1000;
+
+/*  ------------REDUCER CONSTANTS--------------   */
+export enum LoginActions {
+    
+    UPDATE_EMAIL = "UPDATE_EMAIL",
+    UPDATE_PASSWORD = "UPDATE_PASSWORD",
+}
+
+export enum RegisterActions {
+    UPDATE_USERNAME = "UPDATE_USERNAME",
+    UPDATE_EMAIL = "UPDATE_EMAIL",
+    UPDATE_PASSWORD = "UPDATE_PASSWORD",
+    UPDATE_CONFIRM_PASSWORD = "UPDATE_CONFIRM_PASSWORD",
+}
