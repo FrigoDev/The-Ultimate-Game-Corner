bbbf1cdbfab62d8669fbbc37bb058d3396a86393
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 23:18:04 -0500

    🏷️✨Update the useGames hook with params and types

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,60 +1,127 @@
 import getDBGames from "../utils/dbSocket";
 import { debounceTimer } from "../constants/constants";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef,useCallback} from "react";
 import { debounce } from "lodash";
 import { Game } from "../types/Game";
+import { useSearchParams } from "react-router-dom";
+import { paramsToObject } from "../utils/paramsToObject";
+
 const GAMES_PER_PAGE = 10;
-
 const useGames = () => {
+  const [params,setParams]=useSearchParams();
   const [games, setGames] = useState([]as Game[]);
-  const [page, setPage] = useState(1);
-  const [filter, setFilter] = useState("");
   const [totalPages, setTotalPages] = useState(0);
+  const {page,filter:paramfilter}=paramsToObject(params.entries());
+  const [filter,setFilter]= useState<string>("");
 
-  const getGames = async () => {
-    const { games, totalPages: total } = await getDBGames(page, GAMES_PER_PAGE, filter);
-    if (page < 1 && games.length > 0) {
-      return setPage(1);
+  const prevGames=useRef([{
+     page:"0",
+     paramfilter,
+     games,
+     totalPages,
+  },{
+    page:"0",
+    paramfilter,
+    games,
+    totalPages,
+ }])
+
+  useEffect(()=>{
+    const isundef={} as {[key:string]:string};
+    
+    if(!page){
+      isundef.page="1";
     }
-    setGames(games);
-
-    setTotalPages(total);
-    if (page > total) {
-      return setPage(total);
+    if(!paramfilter){
+      isundef.filter="";
     }
-  };
+   if(!page||!paramfilter){
+    setParams({
+      ...{page,filter:paramfilter},
+      ...isundef
+    })
+   }
+    setFilter(paramfilter?paramfilter:"");
+ },[])
+ const getGames = async () => {
+  const cache=prevGames.current.find(oldPage=>oldPage.page===page);
+  if(cache&&cache.paramfilter===paramfilter){
+    setGames(cache.games);
+    setTotalPages(cache.totalPages);
+    return
+  }
+  if (page){
+    const actualPage=Number(page)
+    const { games: newGames, totalPages: total } = await getDBGames(actualPage, GAMES_PER_PAGE, paramfilter?paramfilter:"");
+  if (actualPage < 1 && newGames.length > 0) {
+    return 
+  }
+  setGames(newGames);
+  setTotalPages(total);
+  if (actualPage > total) {
+    return //setPage(total);
+  }
 
-  useEffect(() => {
+  }
+};
+const goOnePageBack = () => {
+  if (Number(page)> 1) {
+    prevGames.current[0] ={
+      ...prevGames.current[0],
+      games,
+      paramfilter,
+      page,
+      totalPages
+    }
+    setParams({page:String( Number(page)-1),
+      filter:paramfilter});
+  }
+};
+
+const goOnePageForward = () => {
+  if (Number(page) < totalPages) {
+    prevGames.current[1] ={
+      ...prevGames.current[1],
+      games,
+      paramfilter,
+      page,
+      totalPages
+    }
+    setParams({
+      page:String( Number(page)+1),
+      filter:paramfilter});
+  }
+};
+ useEffect(()=>{
+    setFilter(paramfilter?paramfilter:"");
     getGames();
-  }, [page, filter]);
+    
+  },[params])
 
-  const goOnePageBack = () => {
-    if (page > 1) {
-      setPage(page - 1);
+const updateFilter = useCallback(debounce((newFilter:string) => {
+    if (newFilter !== paramfilter &&newFilter!=="") {
+      return setParams({
+        page:"1",
+        filter:newFilter
+      })
     }
-  };
-
-  const goOnePageForward = () => {
-    if (page < totalPages) {
-      setPage(page + 1);
+    if (newFilter==="" &&(paramfilter!=="")){
+      return setParams({
+        page:"1",
+        filter:""
+      })
     }
-  };
-
-  const updateFilter =
-    debounce((newFilter:string) => {
-      setFilter(newFilter);
-      setPage(1);
-    }, debounceTimer);
-
+  }, debounceTimer),[params])
+  useEffect(()=>updateFilter(filter!),[filter])
   return {
     games,
-    page,
+    page:Number(page),
     totalPages,
     goOnePageBack,
     goOnePageForward,
-    updateFilter,
+    updateFilter:setFilter,
     filter
-  };
-};
+   } 
+}
 
 export default useGames;
