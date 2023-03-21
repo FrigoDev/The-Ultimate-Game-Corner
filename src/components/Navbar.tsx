1a315b911d46ebbaf8bd96911831e19b5f979117
FrigoAlex <alejo012903@hotmail.com>
2023-03-20 22:34:02 -0500

    🏗️ Add PageRoutes to navbar and implement it

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,72 @@
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import { User } from "../types/user";
+import { PageRoutes } from "../constants/pageRoutes";
 import "../assets/styles/navbar.css";
-import { User } from "../types/user"; 
-import {PageRoutes} from "../constants/pageRoutes"
 interface NavbarProps {
-  user: User|undefined;
-  logout: () =>void
+  user: User | undefined;
+  logout: () => void;
 }
-
 
 const Navbar = ({ user, logout }: NavbarProps) => {
   const navigate = useNavigate();
   return (
     <>
-     <header className="navbar">
-      <div className="logo-container" onClick={() => {
-        navigate(PageRoutes.HOME);
-      }}>
-        <img className="header-logo" src="/Logo-Gamer.png" />
-        <h1 className="header-title">Gamer Corner</h1>
-      </div>
-      <nav className="nav-section">
-        <ul className="nav-list">
-          <li className="nav-elements" onClick={() => {
-            navigate(PageRoutes.HOME);
-          }}>
-            <a className="nav-elements-a">Home</a>
-          </li>
-          {!user
-            ? (
+      <header className="navbar">
+        <div
+          className="logo-container"
+          onClick={() => {
+            navigate(`${PageRoutes.HOME}?page=1`);
+          }}
+        >
+          <img className="header-logo" src="/Logo-Gamer.png" />
+          <h1 className="header-title">Gamer Corner</h1>
+        </div>
+        <nav className="nav-section">
+          <ul className="nav-list">
+            <li
+              className="nav-elements"
+              onClick={() => {
+                navigate(PageRoutes.HOME);
+              }}
+            >
+              <a className="nav-elements-a">Home</a>
+            </li>
+            {user?.name===""? (
               <>
-                <li className="nav-elements" onClick={() => {
-                  navigate("/login");
-                }}>
+                <li
+                  className="nav-elements"
+                  onClick={() => {
+                    navigate(PageRoutes.LOGIN);
+                  }}
+                >
                   <a className="nav-elements-a">Login</a>
                 </li>
                 <li
                   className="nav-elements"
-                  onClick={()=>{
-                    navigate("/register");
+                  onClick={() => {
+                    navigate(PageRoutes.REGISTER);
                   }}
                 >
                   <a className="nav-elements-a">Register</a>
                 </li>
               </>
-            )
-            : (
-              <li className="nav-elements" onClick={async () => {
-                await logout();
-                navigate("/");
-              }}>
-                <a className="nav-elements-a">logout</a>
-              </li>
+            ) : (
+              <>
+                <li
+                  className="nav-elements"
+                  onClick={async () => {
+                    await logout();
+                    navigate(PageRoutes.HOME);
+                  }}
+                >
+                  <a className="nav-elements-a">logout</a>
+                </li>
+              </>
             )}
-        </ul>
-      </nav>
-    </header> 
-</>
-  )
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
 export default Navbar;
