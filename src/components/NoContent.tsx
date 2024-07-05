bb6351c81d4733ea6e626a3d53ccd97d8f853bfc
FrigoAlex <alejo012903@hotmail.com>
2024-07-05 07:47:21 -0500

    ✏️ Fix typos in NoContent component

--- src/components/NoContent.tsx
+++ src/components/NoContent.tsx
@@ -1,9 +1,16 @@
+import React from "react";
 import "../assets/styles/noContent.css";
-const NoContent = ({ text }) => {
+
+interface NoContentProps {
+  text: string;
+}
+
+const NoContent: React.FC<NoContentProps> = ({ text }) => {
   return (
     <div className="no-content">
       <h2 className="no-content-title">{text}</h2>
     </div>
   );
 };
+
 export default NoContent;
